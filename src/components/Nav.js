ecfa1cf1f29b0d9766c336a887da2c1d498a3378
johnnyjuarez <johnnyjuarez1992@gmail.com>
2020-06-01 18:02:22 -0500

    added stylings for add court and find court nav buttons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import { AppBar, Toolbar, Typography, Button, IconButton } from '@material-ui/core';
 import { BrowserRouter, Link } from 'react-router-dom';
 // import MenuIcon from '@material-ui/icons/Menu';
+
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -15,9 +16,17 @@ const useStyles = makeStyles(theme => ({
     flexGrow: 1,
   },
   addButton: {
+    width: '100%',
     color: 'white',
     textDecoration: 'none',
-  }
+    border: '1px solid #2c4966',
+    backgroundImage: 'linear-gradient(bottom, rgb(56, 92, 127) 0%, rgb(64, 105, 145) 100%)',
+    position: 'relative',
+    '&:hover': {
+      backgroundColor: 'white',
+      backgroundImage: 'linear-gradient(bottom, rgb(63, 103, 143) 0%, rgb(75, 124, 173) 100%)',
+    }
+  },
 }));
 
 export default function ButtonAppBar() {
@@ -30,11 +39,11 @@ export default function ButtonAppBar() {
           <Typography variant="h6" className={classes.title}>
             {/* myCourts */}
           </Typography>
-          <Link to="add">
-          <Button className={classes.addButton}>Add Court</Button>
+          <Link to="add" style={{ textDecoration: 'none', marginRight: '20px', padding: '0' }} >
+            <Button className={classes.addButton}>Add Court</Button>
           </Link>
-          <Link to="find">
-          <Button className={classes.addButton}>Find Court</Button>
+          <Link to="find" style={{ textDecoration: 'none', margin: '0', padding: '0' }} >
+            <Button className={classes.addButton}>Find Court</Button>
           </Link>
         </Toolbar>
       </AppBar>
